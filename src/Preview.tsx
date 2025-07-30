f992da2131feed8082d4d131580c4bda4b7ef8ac
sharif24with40 <183331146+sharif24with40@users.noreply.github.com.>
2025-07-30 18:19:24 +0530

    done

--- src/Preview.tsx
+++ src/Preview.tsx
@@ -1,33 +1,45 @@
+import { useEffect, useState } from "react";
 import useCopyToClipboard from "./CopyToClipboard";
 import { marked } from "marked";
 import DOMPurify from "dompurify";
+import './Editor.css'
 
 type PreviewProps = {
   markdown: string;
 };
 
 function Preview({ markdown }: PreviewProps) {
+  const [html, setHtml] = useState("");
   const [copied, copyToClipboard] = useCopyToClipboard();
 
-  return (
-    <div className="preview-section">
-      <div
-        dangerouslySetInnerHTML={{
-          __html: DOMPurify.sanitize(marked(markdown)),
-        }}
-      />
+  useEffect(() => {
+    const convertMarkdown = async () => {
+      const rawHtml = await marked(markdown);
+      const cleanHtml = DOMPurify.sanitize(rawHtml);
+      setHtml(cleanHtml);
+    };
 
-      <div className="copy-container">
-        <button
-          className="copy-button"
-          onClick={() => copyToClipboard(markdown)}
-        >
-          Copy Markdown
-        </button>
-        {copied && <span className="copy-success">Copied!</span>}
-      </div>
+    convertMarkdown();
+  }, [markdown]);
+
+  return (
+  <div className="preview-section">
+    <div
+      className="preview-output"
+      dangerouslySetInnerHTML={{ __html: html }}
+    />
+
+    <div className="copy-container">
+      <button
+        className="copy-button"
+        onClick={() => copyToClipboard(markdown)}
+      >
+        Copy Markdown
+      </button>
+      {copied && <span className="copy-success">Copied!</span>}
     </div>
-  );
+  </div>
+);
 }
 
 export default Preview;
