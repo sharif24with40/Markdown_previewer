512c702d90da39bf410e2c067e2499c40779b4bd
sharif24with40 <183331146+sharif24with40@users.noreply.github.com.>
2025-07-30 18:57:23 +0530

    sdlvm

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.js'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Markdown_previewer">
       <App />
     </BrowserRouter>
   </StrictMode>,
